0167441c131e2dbd9e399ab18a2829074747fd7f
meerathaakur <jayants2859@gmail.com>
2025-03-24 11:02:25 +0530

    bcrypt

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import morgan from "morgan";
 import helmet from "helmet";
 import compression from "compression";
 import path from "path";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
